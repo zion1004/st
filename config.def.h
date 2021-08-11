96e2847074d2656bad4a605cb27efe3b57401dba
Zion Lee <bicoljoy12@gmail.com>
2021-08-11 12:22:27 +0900

    custom-config

--- config.def.h
+++ config.def.h
@@ -5,9 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Noto Sans Mono CJK KR:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
+	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
